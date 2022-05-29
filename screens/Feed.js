72347b90cd3a3107daf35aaebd13d2cc3616489b
prathik402 <73998548+prathik402@users.noreply.github.com>
2022-05-29 08:58:51 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from "react-native-web";
+
+
+export default class Feed extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Feed</Text>
+            </View>
+        )
+    }
+}
